1bdd282a7188804a56e12fb9faf9400d7aff6576
Shiwani <shiwani@Shiwanis-MacBook-Pro.local>
2025-04-30 20:27:24 +1000

    Added Recency Calculation

--- Recency_Calculation.py
+++ Recency_Calculation.py
@@ -1,5 +1,7 @@
+
 import pandas as pd 
-from Recency_Calculation import df
+import datetime
+from Load_Data import df
 
 df["R"] = (pd.Timestamp.now() - pd.to_datetime(df["Date"])).dt.days
 print(df.head())
